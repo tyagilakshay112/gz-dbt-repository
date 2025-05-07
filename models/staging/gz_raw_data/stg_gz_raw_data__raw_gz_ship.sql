26e019a18dcbcfca063c4ad57d4086ac00caa2f8
Lakshay Tyagi <tyagilakshay112@gmail.com>
2025-05-07 09:54:01 +0000

    Challange 4th finance days

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -13,7 +13,7 @@ renamed as (
         shipping_fee,
         shipping_fee_1,
         logcost,
-        ship_cost
+        cast(ship_cost as float64) as ship_cost
 
     from source
 
--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        cast(purchse_price as float64) as purchase_price
+       cast(purchse_price as float64) as purchase_price
 
     from source
 
